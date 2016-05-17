926f6621e668eb031463e50a42f2114bbf27c233
ku00 <skentaro36@gmail.com>
2016-05-17 19:02:09 +0900

    wip level 2.7

--- app.js
+++ app.js
@@ -1,22 +1,56 @@
-(function(){
-  var gems = [
-    {
-      name: 'Dodecahedron',
-      price: 2.95,
-      description: 'I am cat! I am cat! I am cat! I am cat! I am cat! I am cat! I am cat!',
-      canPurchase: true,
-    },
-    {
-      name: 'Pentagonal Gem',
-      price: 5.95,
-      description: 'We are sorry, but something went wrong...',
-      canPurchase: false,
-    },
-  ];
+(function() {
+  var app = angular.module('gemStore', []);
 
-  var app = angular.module('store', []);
-
-  app.controller('StoreController', function(){
+  app.controller('StoreController', function() {
     this.products = gems;
   });
+
+  app.controller('TabController', function(){
+    this.tab = 1;
+
+    this.setTab = function(selectTab){
+      this.tab = selectTab;
+    };
+
+    this.isSet = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
+
+  var gems = [{
+    name: 'Azurite',
+    description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+    shine: 8,
+    price: 110.50,
+    rarity: 7,
+    color: '#CCC',
+    faces: 14,
+    images: [ ]
+  }, {
+    name: 'Bloodstone',
+    description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+    shine: 9,
+    price: 22.90,
+    rarity: 6,
+    color: '#EEE',
+    faces: 12,
+    images: [
+      "images/gem-01.gif",
+      "images/gem-03.gif",
+      "images/gem-04.gif"
+    ]
+  }, {
+    name: 'Zircon',
+    description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+    shine: 70,
+    price: 1100,
+    rarity: 2,
+    color: '#000',
+    faces: 6,
+    images: [
+      "images/gem-06.gif",
+      "images/gem-07.gif",
+      "images/gem-09.gif"
+    ]
+  }];
 })();
