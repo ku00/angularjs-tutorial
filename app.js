337926525bfbc6ef3df834b10142c74f03feeba8
ku00 <skentaro36@gmail.com>
2016-05-17 11:49:38 +0900

    finished level 1

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 (function(){
-  var gem = {
-    name: 'Dodecahedron',
-    price: 2.95,
-    description: '. . .',
-  }
+  var gems = [
+    {
+      name: 'Dodecahedron',
+      price: 2.95,
+      description: 'I am cat! I am cat! I am cat! I am cat! I am cat! I am cat! I am cat!',
+      canPurchase: true,
+    },
+    {
+      name: 'Pentagonal Gem',
+      price: 5.95,
+      description: 'We are sorry, but something went wrong...',
+      canPurchase: false,
+    },
+  ];
 
   var app = angular.module('store', []);
 
   app.controller('StoreController', function(){
-    this.product = gem;
+    this.products = gems;
   });
 })();
