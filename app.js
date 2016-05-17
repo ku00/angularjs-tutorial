2714f28a7f4f8d558ed1bb709f8a1d0ac8bd85fd
ku00 <skentaro36@gmail.com>
2016-05-17 09:59:07 +0900

    wip level 1.5

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-var app = angular.module('store', []);
+(function(){
+  var gem = {
+    name: 'Dodecahedron',
+    price: 2.95,
+    description: '. . .',
+  }
+
+  var app = angular.module('store', []);
+
+  app.controller('StoreController', function(){
+    this.product = gem;
+  });
+})();
