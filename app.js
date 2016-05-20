cc614913a60e27852e238e9094567504ba939418
ku00 <skentaro36@gmail.com>
2016-05-20 09:56:20 +0900

    finished level 4

--- app.js
+++ app.js
@@ -5,26 +5,6 @@
     this.products = gems;
   });
 
-  app.controller('TabController', function(){
-    this.tab = 1;
-
-    this.setTab = function(selectTab){
-      this.tab = selectTab;
-    };
-
-    this.isSet = function(checkTab) {
-      return this.tab === checkTab;
-    };
-  });
-
-  app.controller('GalleryController', function(){
-    this.current = 0;
-
-    this.setCurrent = function(imageNumber){
-      this.current = imageNumber || 0;
-    };
-  });
-
   app.controller('ReviewController', function(){
     this.review = {};
 
@@ -46,6 +26,47 @@
     return {
       restrict: 'A',
       templateUrl: 'product-specs.html'
+    };
+  });
+
+  app.directive('productReviews', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-reviews.html'
+    };
+  });
+
+  app.directive('productTabs', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-tabs.html',
+      controller: function(){
+        this.tab = 1;
+
+        this.setTab = function(selectTab){
+          this.tab = selectTab;
+        };
+
+        this.isSet = function(checkTab) {
+          return this.tab === checkTab;
+        };
+      },
+      controllerAs: 'tab'
+    };
+  });
+
+  app.directive('productGallery', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-gallery.html',
+      controller: function() {
+        this.current = 0;
+
+        this.setCurrent = function(imageNumber){
+          this.current = imageNumber || 0;
+        };
+      },
+      controllerAs: 'gallery'
     };
   });
 
